f332f5e2adb56805e348bf56f27d40c0dd448e7a
bishnunathbd <bishnunath774@gmail.com>
2021-02-08 21:03:00 +0600

    44-2-google sign in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+// Initialize Firebase
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const Login = () => {
+
+  const handleGoogleSignIn = () => {
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  }
+
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <h1>This is Login.</h1>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
     </div>
   );
 };
