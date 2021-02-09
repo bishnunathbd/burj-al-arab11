34bc874c3274adffb227f49021d3ab55df904195
bishnunathbd <bishnunath774@gmail.com>
2021-02-09 11:08:04 +0600

    44-3-create user context to share it with multiple components

--- src/App.js
+++ src/App.js
@@ -10,29 +10,37 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import NotFound from './components/NotFound/NotFound';
 import Book from './components/Book/Book';
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+
   return (
-    <Router>
-      <Header></Header>
-      <Switch>
-        <Route path='/home'>
-          <Home></Home>
-        </Route>
-        <Route path='/login'>
-          <Login></Login>
-        </Route>
-        <Route path='/book/:bedType'>
-          <Book></Book>
-        </Route>
-        <Route exact path='/'>
-          <Home></Home>
-        </Route>
-        <Route path='*'>
-          <NotFound></NotFound>
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <p>Name: {loggedInUser.name}</p>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path='/home'>
+            <Home></Home>
+          </Route>
+          <Route path='/login'>
+            <Login></Login>
+          </Route>
+          <Route path='/book/:bedType'>
+            <Book></Book>
+          </Route>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
 
-// Initialize Firebase
-if (firebase.apps.length === 0) {
-  firebase.initializeApp(firebaseConfig);
-}
+
 
 const Login = () => {
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  // Initialize Firebase
+  if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
 
   const handleGoogleSignIn = () => {
     const googleProvider = new firebase.auth.GoogleAuthProvider();
@@ -16,14 +20,9 @@ const Login = () => {
     firebase.auth()
       .signInWithPopup(googleProvider)
       .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
-        var credential = result.credential;
-
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        console.log(user);
+        const { displayName, email } = result.user;
+        const signedInUser = { name: displayName, email }
+        setLoggedInUser(signedInUser);
       })
       .catch((error) => {
         // Handle Errors here.
